8c47fcd5f4d71a93b557bb3ee92b0b4bcaa1825f
Michael Kirsch <mkirsch.git@gmail.com>
2022-05-19 23:03:11 +0200

    GP variable names

--- update-lovelace-cards.sh
+++ update-lovelace-cards.sh
@@ -9,59 +9,59 @@ PATHPREFIX=/local/lovelace-custom/
 cd "$SCRIPTPATH/www/lovelace-custom"
 
 # https://github.com/thomasloven/lovelace-fold-entity-row
-FER_REPO="thomasloven/lovelace-fold-entity-row"
-FER_FILE="fold-entity-row.js"
-FER_TAG=$(curl -s https://api.github.com/repos/$FER_REPO/releases/latest | jq -r '.tag_name')
-curl -O "https://raw.githubusercontent.com/$FER_REPO/$FER_TAG/$FER_FILE" 2>/dev/null
-echo "$PATHPREFIX$FER_FILE?v=$FER_TAG" > "$SCRIPTPATH/$VERSIONSFILE"
+REPO="thomasloven/lovelace-fold-entity-row"
+FILE="fold-entity-row.js"
+TAG=$(curl -s https://api.github.com/repos/$REPO/releases/latest | jq -r '.tag_name')
+curl -O "https://raw.githubusercontent.com/$REPO/$TAG/$FILE" 2>/dev/null
+echo "$PATHPREFIX$FILE?v=$TAG" > "$SCRIPTPATH/$VERSIONSFILE"
 
 # https://github.com/thomasloven/lovelace-layout-card
-LC_REPO="thomasloven/lovelace-layout-card"
-LC_FILE="layout-card.js"
-LC_TAG=$(curl -s https://api.github.com/repos/$LC_REPO/releases/latest | jq -r '.tag_name')
-curl -O "https://raw.githubusercontent.com/$LC_REPO/$LC_TAG/$LC_FILE" 2>/dev/null
-echo "$PATHPREFIX$LC_FILE?v=$LC_TAG" >> "$SCRIPTPATH/$VERSIONSFILE"
+REPO="thomasloven/lovelace-layout-card"
+FILE="layout-card.js"
+TAG=$(curl -s https://api.github.com/repos/$REPO/releases/latest | jq -r '.tag_name')
+curl -O "https://raw.githubusercontent.com/$REPO/$TAG/$FILE" 2>/dev/null
+echo "$PATHPREFIX$FILE?v=$TAG" >> "$SCRIPTPATH/$VERSIONSFILE"
 
 # https://github.com/kalkih/mini-graph-card
 # on raw tags, download might break
-MG_REPO="kalkih/mini-graph-card"
-MG_FILE="mini-graph-card-bundle.js"
-MG_TAG=$(curl -s https://api.github.com/repos/$MG_REPO/tags | jq -r 'first | .name')
-curl -L -O "https://github.com/$MG_REPO/releases/download/$MG_TAG/$MG_FILE" 2>/dev/null
-echo "$PATHPREFIX$MG_FILE?v=$MG_TAG" >> "$SCRIPTPATH/$VERSIONSFILE"
+REPO="kalkih/mini-graph-card"
+FILE="mini-graph-card-bundle.js"
+TAG=$(curl -s https://api.github.com/repos/$REPO/tags | jq -r 'first | .name')
+curl -L -O "https://github.com/$REPO/releases/download/$TAG/$FILE" 2>/dev/null
+echo "$PATHPREFIX$FILE?v=$TAG" >> "$SCRIPTPATH/$VERSIONSFILE"
 
 # https://github.com/kalkih/mini-media-player
-MP_REPO="kalkih/mini-media-player"
-MP_FILE="mini-media-player-bundle.js"
-MP_TAG=$(curl -s https://api.github.com/repos/$MP_REPO/releases/latest | jq -r '.tag_name')
-curl -L -O "https://github.com/$MP_REPO/releases/download/$MP_TAG/$MP_FILE" 2>/dev/null
-echo "$PATHPREFIX$MP_FILE?v=$MP_TAG" >> "$SCRIPTPATH/$VERSIONSFILE"
+REPO="kalkih/mini-media-player"
+FILE="mini-media-player-bundle.js"
+TAG=$(curl -s https://api.github.com/repos/$REPO/releases/latest | jq -r '.tag_name')
+curl -L -O "https://github.com/$REPO/releases/download/$TAG/$FILE" 2>/dev/null
+echo "$PATHPREFIX$FILE?v=$TAG" >> "$SCRIPTPATH/$VERSIONSFILE"
 
 # https://github.com/benct/lovelace-multiple-entity-row
-MER_REPO="benct/lovelace-multiple-entity-row"
-MER_FILE="multiple-entity-row.js"
-MER_TAG=$(curl -s https://api.github.com/repos/$MER_REPO/releases/latest | jq -r '.tag_name')
-curl -O "https://raw.githubusercontent.com/$MER_REPO/$MER_TAG/$MER_FILE" 2>/dev/null
-echo "$PATHPREFIX$MER_FILE?v=$MER_TAG" >> "$SCRIPTPATH/$VERSIONSFILE"
+REPO="benct/lovelace-multiple-entity-row"
+FILE="multiple-entity-row.js"
+TAG=$(curl -s https://api.github.com/repos/$REPO/releases/latest | jq -r '.tag_name')
+curl -O "https://raw.githubusercontent.com/$REPO/$TAG/$FILE" 2>/dev/null
+echo "$PATHPREFIX$FILE?v=$TAG" >> "$SCRIPTPATH/$VERSIONSFILE"
 
 # https://github.com/thomasloven/lovelace-slider-entity-row
 # on raw tags
-SER_REPO="thomasloven/lovelace-slider-entity-row"
-SER_FILE="slider-entity-row.js"
-SER_TAG=$(curl -s https://api.github.com/repos/$SER_REPO/tags | jq -r '.[] | select(.name|test("^\\d.*";"g")) .name' | head -n 1)
-curl -O "https://raw.githubusercontent.com/$SER_REPO/$SER_TAG/$SER_FILE" 2>/dev/null
-echo "$PATHPREFIX$SER_FILE?v=$SER_TAG" >> "$SCRIPTPATH/$VERSIONSFILE"
+REPO="thomasloven/lovelace-slider-entity-row"
+FILE="slider-entity-row.js"
+TAG=$(curl -s https://api.github.com/repos/$REPO/tags | jq -r '.[] | select(.name|test("^\\d.*";"g")) .name' | head -n 1)
+curl -O "https://raw.githubusercontent.com/$REPO/$TAG/$FILE" 2>/dev/null
+echo "$PATHPREFIX$FILE?v=$TAG" >> "$SCRIPTPATH/$VERSIONSFILE"
 
 # https://github.com/ofekashery/vertical-stack-in-card
-SIC_REPO="ofekashery/vertical-stack-in-card"
-SIC_FILE="stack-in-card.js"
-SIC_TAG=$(curl -s https://api.github.com/repos/$SIC_REPO/releases/latest | jq -r '.tag_name')
-curl "https://raw.githubusercontent.com/$SIC_REPO/$SIC_TAG/vertical-stack-in-card.js" 2>/dev/null | sed s/vertical-stack-in-card/stack-in-card/ > "$SIC_FILE"
-echo "$PATHPREFIX$SIC_FILE?v=$SIC_TAG" >> "$SCRIPTPATH/$VERSIONSFILE"
+REPO="ofekashery/vertical-stack-in-card"
+FILE="stack-in-card.js"
+TAG=$(curl -s https://api.github.com/repos/$REPO/releases/latest | jq -r '.tag_name')
+curl "https://raw.githubusercontent.com/$REPO/$TAG/vertical-stack-in-card.js" 2>/dev/null | sed s/vertical-stack-in-card/stack-in-card/ > "$FILE"
+echo "$PATHPREFIX$FILE?v=$TAG" >> "$SCRIPTPATH/$VERSIONSFILE"
 
 # https://github.com/thomasloven/lovelace-template-entity-row
-TER_REPO="thomasloven/lovelace-template-entity-row"
-TER_FILE="template-entity-row.js"
-TER_TAG=$(curl -s https://api.github.com/repos/$TER_REPO/releases/latest | jq -r '.tag_name')
-curl -O "https://raw.githubusercontent.com/$TER_REPO/$TER_TAG/$TER_FILE" 2>/dev/null
-echo "$PATHPREFIX$TER_FILE?v=$TER_TAG" >> "$SCRIPTPATH/$VERSIONSFILE"
+REPO="thomasloven/lovelace-template-entity-row"
+FILE="template-entity-row.js"
+TAG=$(curl -s https://api.github.com/repos/$REPO/releases/latest | jq -r '.tag_name')
+curl -O "https://raw.githubusercontent.com/$REPO/$TAG/$FILE" 2>/dev/null
+echo "$PATHPREFIX$FILE?v=$TAG" >> "$SCRIPTPATH/$VERSIONSFILE"
